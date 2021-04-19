e06bcea96dc964d0aa6584a378e7eefb2d5ce044
Sebastien <sebastien.ambona33@gmail.com>
2021-04-19 21:11:32 +0200

    Premier commit

---
+++ controleurs/controleurPrincipal.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'lib/dispatcher.php';
+// Verification  pour voir si il y'a déja un menu principale ou non 
+if (isset($_GET['vlibMP'])){
+    $_SESSION['vlibMP']= $_GET['vlibMP'];
+}
+// sinon affecte au menu principal la valeur par defaut
+else{
+    if(!isset($_SESSION['vlibMP'])){
+        $_SESSION['vlibMP']="accueil";
+    }
+}
+
+//Création du menu principale
+
+
+$vlibMP = new Menu("menuPrincipal");
+$vlibMP->ajouterComposant($vlibMP->creerItemLien("accueil", "Accueil"));
+$vlibMP->ajouterComposant($vlibMP->creerItemLien("stations", "Stations"));
+$vlibMP->ajouterComposant($vlibMP->creerItemLien("abonnement", "Abonnement"));
+$vlibMP->ajouterComposant($vlibMP->creerItemLien("abonnement", "Nous contacter"));
+$menuPrincipal = $vlibMP->creerMenu($_SESSION['vlibMP'],'vlibMP');
+
+include_once dispatcher::dispatch($_SESSION['vlibMP']);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'lib/autoLoader.php';
+	require_once 'lib/dispatcher.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>Vlib</title>
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Varela+Round:ital,wght@1,300&display=swap" rel="stylesheet"> 
+		<link rel="stylesheet" href="styles/style.css">
+
+	</head>
+	<body >
+		<?php
+			require_once 'controleurs/controleurPrincipal.php';	
+		?>
+	</body>
+</html>
